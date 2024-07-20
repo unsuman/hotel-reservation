5ad414bb646a87c657738f22de888e8e8a10bbee
Ansuman Sahoo <anshumansahoo500@gmail.com>
2024-07-21 01:45:01 +0530

    Add test for Update User

--- api/user_handle_test.go
+++ api/user_handle_test.go
@@ -50,8 +50,10 @@ func TestUserCRUD(t *testing.T) {
 
 	defer setup().teardown(t)
 
-	var user types.User
-
+	var (
+		user   types.User
+		userID string
+	)
 	t.Run("TestPostUser", func(t *testing.T) {
 
 		app.Post("/", userHandler.HandlePostUser)
@@ -92,7 +94,7 @@ func TestUserCRUD(t *testing.T) {
 	t.Run("TestGetUser", func(t *testing.T) {
 		app.Get("/:id", userHandler.HandleGetUser)
 
-		userID := "/" + user.ID.Hex()
+		userID = "/" + user.ID.Hex()
 
 		req := httptest.NewRequest("GET", userID, nil)
 		resp, err := app.Test(req)
@@ -118,6 +120,36 @@ func TestUserCRUD(t *testing.T) {
 			t.Errorf("expected email %s but got %s", singleUser.Email, user.Email)
 		}
 
+	})
+
+	type Status struct {
+		Updated string `json:"updated"`
+	}
+
+	t.Run("TestUpdateUser", func(t *testing.T) {
+		app.Put("/:id", userHandler.HandleUpdateUser)
+
+		userUpdate := &types.UpdateUserParams{
+			FirstName: "Aditya",
+			LastName:  "At the beach",
+		}
+
+		b, _ := json.Marshal(userUpdate)
+
+		req := httptest.NewRequest("PUT", userID, bytes.NewReader(b))
+		req.Header.Add("Content-type", "application/json")
+		resp, err := app.Test(req)
+		if err != nil {
+			t.Error(err)
+		}
+
+		var stat Status
+
+		json.NewDecoder(resp.Body).Decode(&stat)
+
+		if stat.Updated != user.ID.Hex() {
+			t.Errorf("Error updating %s", user.ID.Hex())
+		}
 	})
 
 }
